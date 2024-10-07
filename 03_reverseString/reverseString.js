24e4ad21a3771d24243535c7bfdf83bed3f8e24b
Black Shadow <mithabozdar2@gmail.com>
2024-10-07 17:27:33 +0500

    Add solution to the 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Create an empty string variable to store the reversed string
+    let temp_str = "";
 
+    // Go through each character of the string and append them in the starting position of the temp_str variable
+    for (let i = 0; i < string.length; ++i) {
+        temp_str = string[i] + temp_str;
+    }
+    
+    // The result is the reversed version of the original string
+    return temp_str;
 };
 
 // Do not edit below this line

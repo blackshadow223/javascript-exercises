ef3875beaf3cbf4a9b109eeabae040af83b77bf5
Black Shadow <mithabozdar2@gmail.com>
2024-10-07 17:09:36 +0500

    Add solution to the 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num=1) {
+    if (num === 0) return "";
+    if (num < 0) return "ERROR";
+    if (string === "") return "";
+    const temp_str = string;
+    for (let i = 1; i < num; ++i) {
+        string += temp_str;
+    }
+    return string;
 };
 
 // Do not edit below this line

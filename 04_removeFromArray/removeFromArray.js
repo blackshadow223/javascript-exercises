ac21980edd3a490b8e453fc6dd7d9e0335fc8099
Black Shadow <mithabozdar2@gmail.com>
2024-10-08 16:24:33 +0500

    Add solution to the 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i < args.length; ++i) {
+        let index = array.indexOf(args[i]);
+        while (index !== -1) {
+            array.splice(index, 1);
+            index = array.indexOf(args[i]);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line

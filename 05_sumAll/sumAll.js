6d21d0a58ae82e79ce56d1dbbee5ae3ebfb4c5e9
Black Shadow <mithabozdar2@gmail.com>
2024-10-08 17:27:54 +0500

    Add solution to the 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    if (typeof(start) !== "number" || typeof(end) !== "number") return "ERROR";
+    else if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    else if (start < 0 || end < 0) return "ERROR";
+    else if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    
+    // Sum of All numbers
+    let sum = 0;
+    for (let i = start; i <= end; ++i) sum += i;
+    return sum;
 };
 
 // Do not edit below this line

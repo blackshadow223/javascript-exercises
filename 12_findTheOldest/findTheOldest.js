9a9112bbae68e95beeadadecc8a0d2d8653007b8
Black Shadow <mithabozdar2@gmail.com>
2024-10-18 20:58:11 +0500

    Add solution to the 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.sort((prevObj, nextObj) => {
+        let prevEndYear = prevObj.yearOfDeath ? prevObj.yearOfDeath : (new Date()).getFullYear();
+        let nextEndYear = nextObj.yearOfDeath ? nextObj.yearOfDeath : (new Date()).getFullYear();
 
+        let prevAge = prevObj.yearOfBirth - prevEndYear;
+        let nextAge = nextObj.yearOfBirth - nextEndYear;
+
+        return prevAge - nextAge;
+    }).at(0);
 };
 
 // Do not edit below this line

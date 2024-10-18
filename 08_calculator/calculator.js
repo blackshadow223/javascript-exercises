0f4beb6c0b75b660997b9e6054c1b231decc347c
Black Shadow <mithabozdar2@gmail.com>
2024-10-18 19:02:53 +0500

    Add solution to the 8th exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((value, number) => (value + number), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((value, number) => (value * number), 1);
 };
 
-const power = function() {
-	
+const power = function (firstNumber, secondNumber) {
+  return firstNumber ** secondNumber;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let value = 1;
+  for (let i = 1; i <= number; ++i) {
+    value *= i;
+  }
+  return value;
 };
 
 // Do not edit below this line

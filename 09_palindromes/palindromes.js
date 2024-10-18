5e472dcaa0beac93a9c201510c2f43f2ad7a131d
Black Shadow <mithabozdar2@gmail.com>
2024-10-18 19:56:43 +0500

    Add solution to the 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const PUNCTUATION = ['!', ',', '.'];
 
+    const regularString = string
+        .split('')
+        .reduce((value, next) => {
+            if (next === ' ' || PUNCTUATION.includes(next))
+                return value;
+            return value + next;
+        }, '')
+        .toLowerCase();
+
+    const reverseString = regularString.split('').reverse().join('');
+    return regularString === reverseString;
 };
 
 // Do not edit below this line

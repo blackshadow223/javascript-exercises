b464028ff7e7b800b907c9f2891228018f803279
Black Shadow <mithabozdar2@gmail.com>
2024-10-18 20:33:59 +0500

    Add solution to the 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    number = parseInt(number);
 
+    if (number === 0) return 0;
+    if (number < 0) return "OOPS";
+
+    let prevNumber = 0;
+    let nextNumber = 1;
+    
+    for (let i = 1; i < number; ++i) {
+        let temp = nextNumber;
+        nextNumber = prevNumber + nextNumber;
+        prevNumber = temp;
+    }
+    
+    return nextNumber;
 };
 
 // Do not edit below this line
